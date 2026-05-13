309f7ab05d5e741ae175a1e8920cf9b8eead8914
Thomas Bittner <thomasbittner@hotmail.de>
2026-05-13 10:19:27 +0200

    more cleanups

--- cross_camera_matcher.py
+++ cross_camera_matcher.py
@@ -28,15 +28,15 @@ class CrossCameraMatcher:
         self.embedding_size = embedder.embedding_dim
         self.not_from_other_camera_masks = not_from_other_camera_masks
 
-        self.good_crops_per_ids_0 = defaultdict(list)
-        self.bad_crops_per_ids_0 = defaultdict(list)
-        self.embeddings_of_crossed_per_id_0 = {}
-        self.histograms_of_crossed_0 = {}
-        self.embedding_histories_1 = defaultdict(list)
-        self.embedding_of_crossed_0_map = []
-        self.embedding_of_crossed_0 = np.zeros(0)
+        self.good_crops_per_ids_source = defaultdict(list)
+        self.bad_crops_per_ids_source = defaultdict(list)
+        self.embeddings_of_crossed_per_id_source = {}
+        self.histograms_of_crossed_source = {}
+        self.embedding_histories_query = defaultdict(list)
+        self.embedding_of_crossed_source_map = []
+        self.embedding_of_crossed_source = np.zeros(0)
         self.comes_from_other_query_camera = {}
-        self.best_matches_1 = defaultdict(list)
+        self.best_matches_query = defaultdict(list)
         self.query_camera_overlap_by_track_id = {}
 
     def store_query_camera_embeddings(self, tracks, frame):
@@ -81,7 +81,7 @@ class CrossCameraMatcher:
 
         for index, vector in enumerate(current_embeddings):
             track_id = non_overlapping_track_ids[index]
-            self.embedding_histories_1[track_id].append(vector)
+            self.embedding_histories_query[track_id].append(vector)
 
     def query_camera_track_is_relevant(self, track_id):
         return self.comes_from_other_query_camera.get(track_id, True)
@@ -94,87 +94,87 @@ class CrossCameraMatcher:
         crop = geometry_utils.get_shrunk_crop(frame, x1, y1, x2, y2, scale=0.8)
 
         if not is_overlapping and width > 90:
-            self.good_crops_per_ids_0[track_id].append(crop)
+            self.good_crops_per_ids_source[track_id].append(crop)
             return True
 
-        self.bad_crops_per_ids_0[track_id].append(crop)
+        self.bad_crops_per_ids_source[track_id].append(crop)
         return False
 
     def record_source_camera_crossing(self, track_id):
-        crops = self.good_crops_per_ids_0[track_id] or self.bad_crops_per_ids_0[track_id]
+        crops = self.good_crops_per_ids_source[track_id] or self.bad_crops_per_ids_source[track_id]
         embedding = calculate_embedding_multiple(self.embedder, crops)
         if embedding is None:
             return
 
-        self.embeddings_of_crossed_per_id_0[track_id] = embedding
-        self.histograms_of_crossed_0[track_id] = color_utils.calculate_histograms_multiple(crops)
+        self.embeddings_of_crossed_per_id_source[track_id] = embedding
+        self.histograms_of_crossed_source[track_id] = color_utils.calculate_histograms_multiple(crops)
 
     def refresh_source_camera_gallery(self):
-        self.embedding_of_crossed_0 = np.zeros((len(self.embeddings_of_crossed_per_id_0), self.embedding_size), dtype="float64")
-        self.embedding_of_crossed_0_map.clear()
+        self.embedding_of_crossed_source = np.zeros((len(self.embeddings_of_crossed_per_id_source), self.embedding_size), dtype="float64")
+        self.embedding_of_crossed_source_map.clear()
 
-        for index, other_track_id in enumerate(sorted(self.embeddings_of_crossed_per_id_0.keys())):
-            self.embedding_of_crossed_0[index] = self.embeddings_of_crossed_per_id_0[other_track_id]
-            self.embedding_of_crossed_0_map.append(other_track_id)
+        for index, other_track_id in enumerate(sorted(self.embeddings_of_crossed_per_id_source.keys())):
+            self.embedding_of_crossed_source[index] = self.embeddings_of_crossed_per_id_source[other_track_id]
+            self.embedding_of_crossed_source_map.append(other_track_id)
 
     def update_query_camera_matches(self, track_id, crossed_times_by_camera):
         is_overlapping = self.query_camera_overlap_by_track_id.get(track_id)
-        if is_overlapping or not self.embedding_histories_1[track_id]:
+        if is_overlapping or not self.embedding_histories_query[track_id]:
             return
 
-        query_embedding = np.mean(self.embedding_histories_1[track_id], axis=0)
-        if self.embedding_of_crossed_0.size == 0 or not self.embedding_of_crossed_0_map:
+        query_embedding = np.mean(self.embedding_histories_query[track_id], axis=0)
+        if self.embedding_of_crossed_source.size == 0 or not self.embedding_of_crossed_source_map:
             return
 
         closest_indices, embedding_scores = embedding_utils.find_closest_embeddings(
             query_embedding,
-            self.embedding_of_crossed_0,
+            self.embedding_of_crossed_source,
         )
         if not closest_indices:
             return
 
         for closest_index, embedding_score in zip(closest_indices, embedding_scores):
-            other_track_id = self.embedding_of_crossed_0_map[closest_index]
+            other_track_id = self.embedding_of_crossed_source_map[closest_index]
             other_draw_crop = self._best_crop_for_source_camera_track(other_track_id)
 
             elapsed_time = -1.0
             if track_id in crossed_times_by_camera[1]:
                 elapsed_time = crossed_times_by_camera[1][track_id] - crossed_times_by_camera[0][other_track_id]
 
-            self._upsert_match(track_id, other_track_id, embedding_score, other_draw_crop, elapsed_time)
+            match_updated = False
+            for match_data in self.best_matches_query[track_id]:
+                if match_data["other_track_id"] == other_track_id:
+                    match_data["embedding_score"] = embedding_score
+                    if elapsed_time != -1.0:
+                        match_data["elapsed_time"] = elapsed_time
+                    match_updated = True
+                    break
+
+            if not match_updated:
+                self.best_matches_query[track_id].append(
+                    {
+                        "embedding_score": embedding_score,
+                        "other_draw_crop": other_draw_crop,
+                        "other_track_id": other_track_id,
+                        "elapsed_time": elapsed_time,
+                    }
+                )
 
     def update_query_camera_elapsed_times(self, track_id, crossed_times_by_camera):
-        if track_id not in self.best_matches_1 or track_id not in crossed_times_by_camera[1]:
+        if track_id not in self.best_matches_query or track_id not in crossed_times_by_camera[1]:
             return
 
-        for match_data in self.best_matches_1[track_id]:
+        for match_data in self.best_matches_query[track_id]:
             if match_data["elapsed_time"] == -1.0:
                 other_track_id = match_data["other_track_id"]
                 match_data["elapsed_time"] = crossed_times_by_camera[1][track_id] - crossed_times_by_camera[0][other_track_id]
 
-        self.best_matches_1[track_id].sort(key=lambda x: x["embedding_score"], reverse=True)
+        self.best_matches_query[track_id].sort(key=lambda x: x["embedding_score"], reverse=True)
 
     def _best_crop_for_source_camera_track(self, track_id):
-        if self.good_crops_per_ids_0[track_id]:
-            self.good_crops_per_ids_0[track_id].sort(key=lambda crop: crop.shape[1])
-            return self.good_crops_per_ids_0[track_id][-1]
+        if self.good_crops_per_ids_source[track_id]:
+            self.good_crops_per_ids_source[track_id].sort(key=lambda crop: crop.shape[1])
+            return self.good_crops_per_ids_source[track_id][-1]
 
-        self.bad_crops_per_ids_0[track_id].sort(key=lambda crop: crop.shape[1])
-        return self.bad_crops_per_ids_0[track_id][-1]
-
-    def _upsert_match(self, track_id, other_track_id, embedding_score, other_draw_crop, elapsed_time):
-        for match_data in self.best_matches_1[track_id]:
-            if match_data["other_track_id"] == other_track_id:
-                match_data["embedding_score"] = embedding_score
-                if elapsed_time != -1.0:
-                    match_data["elapsed_time"] = elapsed_time
-                return
-
-        self.best_matches_1[track_id].append(
-            {
-                "embedding_score": embedding_score,
-                "other_draw_crop": other_draw_crop,
-                "other_track_id": other_track_id,
-                "elapsed_time": elapsed_time,
-            }
-        )
+        self.bad_crops_per_ids_source[track_id].sort(key=lambda crop: crop.shape[1])
+        return self.bad_crops_per_ids_source[track_id][-1]
